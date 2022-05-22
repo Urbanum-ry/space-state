ce23f48b713e37363dc68427d5318b670fe46b26
rowan8k <40304788+rowan8k@users.noreply.github.com>
2022-05-22 21:16:23 +0300

    Add files via upload

---
+++ spacestate_mqtt.ino
@@ -0,0 +1,48 @@
+#include "EspMQTTClient.h"
+#include "FastLED.h"
+
+#define NUM_LEDS 25
+#define LED_PIN 27
+
+CRGB leds [NUM_LEDS];
+
+EspMQTTClient client(
+  "Sector ZZ9 Plural Z Alpha",
+  "Pl4n3t34rth!",
+  "10.47.45.113",
+  "Urbanum_door"
+);
+
+void setup() {
+  pinMode(33, INPUT);
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(50);
+}
+
+void onConnectionEstablished() {}
+
+void loop() { 
+    
+    client.loop();
+    
+    static bool oldstate = !digitalRead(33);
+
+    bool state = digitalRead(33);
+
+    if (state != oldstate) {
+      client.publish("/hervanta/main_building/urbanum", state ? "on" : "off");
+    }
+    
+    if (state == 0) {
+      fill_solid(leds, NUM_LEDS, CRGB::Red);
+      FastLED.show();
+    }
+
+    if (state == 1) {
+      fill_solid(leds, NUM_LEDS, CRGB::Green);
+      FastLED.show();
+    }
+
+    oldstate = state;
+    delay(100);
+}
